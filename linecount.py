0bacf6b105e4f38951b9fb0aedbffb582f9b053c
superobserver <j.w.helkenberg@gmail.com>
2016-08-13 09:39:06 -0500

    Delete linecount.py

--- linecount.py
+++
@@ -1,9 +0,0 @@
-import collections
-
-#run this program from command line as "pylinecount.py >> ballard.txt
-
-
-with open('composite2.csv') as infile:
-    counts = collections.Counter(l.strip() for l in infile)
-for line, count in counts.most_common():
-    print line, count
