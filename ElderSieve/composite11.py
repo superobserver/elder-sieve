c8a8bf14f5fed10335b14c0fa9b880d92a0f2300
Jeffrey W. Helkenberg <j.w.helkenberg@gmail.com>
2017-12-12 23:08:29 -0600

    Update composite11.py

--- ElderSieve/composite11.py
+++ ElderSieve/composite11.py
@@ -4,7 +4,6 @@
 new_test = 1000000
 a = range(0, new_test)
 
-
 def drLD_min_min(x, l, m, z, o):      
   y = 90*(x*x) - l*x - m 
   if y < new_test:
@@ -42,10 +41,8 @@ def drLD_min_plus(x, l, m, z, o):
     else:
       pass
 
-
 for x in xrange(1, 108): # 10000 = 12; 100000 = 36; 1,000,000 = 108; 10,000,000 = 336; 100,000,000 = 1056; 1,000,000,000 = 
- 
- 
+  
     drLD_min_min(x, 78, 1, 11, 91)   #11,91
     drLD_min_plus(x, 132, 48, 19, 29) #19,29
     drLD_min_plus(x, 60, 4, 37, 83)   #37,83
@@ -59,7 +56,6 @@ for x in xrange(1, 108): # 10000 = 12; 100000 = 36; 1,000,000 = 108; 10,000,000 
     drLD_min_plus(x, 48, 6, 61, 71)   #61,71
     drLD_min_plus(x, 12, 0, 79, 89)   #79,89
 
-
 #print a  
 if a[0] == 0:
   list_A201804 = [i for i in a if i !=0]
@@ -70,7 +66,6 @@ if a[0] > 0:
   list_A201804 = [i for i in a if i !=0]
   print list_A201804
   print "These are the first %d terms of Sloane's A224855." % len(list_A201804)
-  
   
 # #Use this code to search for Arithmetic Progressions of Twin Primes.
 # A = list_A201804    # in sorted order
